b11981cf49350ca40ce341dae1f84f2d9247de77
Dickson Palomeras <dicksonpal_021@yahoo.com.ph>
2022-04-03 16:36:45 +0800

    Add firestore config, ballot component and candidates json file

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -4,7 +4,12 @@ import { getFirestore, query, getDocs, collection, where, addDoc } from "firebas
 
 
 const firebaseConfig = {
- // api keys mo dito bebebebe hahaahah
+  apiKey: 'AIzaSyA9TRcmGNCC8zF1YEZB67chxeBwK5Xp4HU',
+  authDomain: 'react-firebase-auth-93c06.firebaseapp.com',
+  projectId: 'react-firebase-auth-93c06',
+  storageBucket: 'react-firebase-auth-93c06.appspot.com',
+  messagingSenderId: '388513152774',
+  appId: '1:388513152774:web:8513c438ce7c0402fee887',
 };
 
 
